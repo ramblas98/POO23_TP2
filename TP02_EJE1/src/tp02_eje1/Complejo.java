e5d5a5dc8f5f4fa5b467b4bfed45234f7ef52435
Sans <Sans@Sans-PC>
2023-04-10 16:59:27 -0300

    primera version

---
+++ TP02_EJE1/src/tp02_eje1/Complejo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp02_eje1;
+
+/**
+ *
+ * @author Sans
+ */
+public class Complejo {
+    private double re;
+    private double im;
+
+    public Complejo() {
+    }
+
+    public Complejo(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public void setIm(double im) {
+        this.im = im;
+    }
+    
+    
+    
+}
---
+++ TP02_EJE1/src/tp02_eje1/TP02_EJE1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp02_eje1;
+
+/**
+ *
+ * @author Sans
+ */
+public class TP02_EJE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
