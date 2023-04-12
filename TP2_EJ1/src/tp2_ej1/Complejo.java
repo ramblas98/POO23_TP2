6412d890d4bb6e5f251ee014eb43c6b74a78bae8
medin <medin@DESKTOP-F911VBP>
2023-04-12 15:20:54 -0300

    Commit final TP2

--- TP2_EJ1/src/tp2_ej1/Complejo.java
+++ TP2_EJ1/src/tp2_ej1/Complejo.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tp2_ej1;
-
 /**
  *
  * @author medin
@@ -35,7 +34,52 @@ public class Complejo {
     public void setIm(double im) {
         this.im = im;
     }
+
+    @Override
+    public String toString() {
+        return "Complejo{" + "re=" + re + ", im=" + im + '}';
+    }
     
+    public void inc(Complejo a){
+        this.re=this.re+a.re;
+        this.im=this.im+a.im;
+    }
     
+    public void dec(Complejo a){
+        this.re-=a.re;
+        this.im-=a.im;
+    }
     
+    public void por(Complejo a){
+        this.re*=a.re;
+        this.im*=a.im;
+    }
+    public void por(double d){
+        this.re*=d;
+        this.im*=d;
+    }
+    public void div(Complejo a){
+        this.re/=a.re;
+        this.im/=a.im;
+    }
+    public double modulo(){
+        return Math.sqrt( Math.pow(this.re,2) + Math.pow(this.im,2) );
+    }
+    public Complejo conjugado(){
+        return new Complejo(this.re,this.im*-1);
+    }
+    
+    public static Complejo suma(Complejo a,Complejo b){
+        return new Complejo(a.re+b.re,a.im+b.im);
+    }
+    
+    public static Complejo resta(Complejo a,Complejo b){
+        return new Complejo(a.re-b.re,a.im-b.im);
+    }
+    public static Complejo producto(Complejo a,Complejo b){
+        return new Complejo(a.re*b.re,a.im*b.im);
+    }
+    public static Complejo division(Complejo a,Complejo b){
+        return new Complejo(a.re/b.re,a.im/b.im);
+    }
 }
--- TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
+++ TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
@@ -14,7 +14,17 @@ public class TP2_EJ1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Complejo c1,c2,c3;
+        
+        c1 = new Complejo(1,2);
+        c2 = new Complejo(2,3);
+        double d=2;
+        
+        c3 = Complejo.division(c1, c2);
+        System.out.println(c3);
+        
+        
     }
     
 }
