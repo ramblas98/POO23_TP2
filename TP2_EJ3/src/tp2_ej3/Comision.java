6960984586f88afb1329ada98f604046c4c5367d
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-04-15 21:07:24 -0300

    Comision

---
+++ TP2_EJ3/src/tp2_ej3/Comision.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej3;
+
+
+public class Comision {
+    private int totalClases;
+    private Estudiante[] listaEstudiantes;
+    private int totalEstudiante;
+
+    public Comision() {
+    }
+
+    public Comision(int totalClases, Estudiante[] listaEstudiantes, int totalEstudiante) {
+        this.totalClases = totalClases;
+        this.listaEstudiantes = listaEstudiantes;
+        this.totalEstudiante = totalEstudiante;
+    }
+
+    public int getTotalClases() {
+        return totalClases;
+    }
+
+    public Estudiante[] getListaEstudiantes() {
+        return listaEstudiantes;
+    }
+
+    public int getTotalEstudiante() {
+        return totalEstudiante;
+    }
+
+    public void setTotalClases(int totalClases) {
+        this.totalClases = totalClases;
+    }
+
+    public void setListaEstudiantes(Estudiante[] listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
+    }
+
+    public void setTotalEstudiante(int totalEstudiante) {
+        this.totalEstudiante = totalEstudiante;
+    }
+    
+   public void agregarEstudiante(){
+       Estudiante e = new Estudiante();
+       this.listaEstudiantes[this.totalEstudiante] = e;
+       this.totalEstudiante++;
+   }
+   
+}
