b801933fe7ed1bff9f9530626bfe2643691b69e2
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-04-16 23:50:03 -0300

    mostrarRegulares

--- TP2_EJ3/src/tp2_ej3/Comision.java
+++ TP2_EJ3/src/tp2_ej3/Comision.java
@@ -49,4 +49,72 @@ public class Comision {
        this.totalEstudiante++;
    }
    
+   public void mostrarLista(){
+       System.out.println("Lista de estudiantes:");
+       for(int i=0 ; i<this.totalEstudiante; i++){
+           Estudiante e = this.listaEstudiantes[i];
+           System.out.println("Estudiante #" + (i+1));
+           System.out.println("Apellido: "+e.getApellido());
+           System.out.println("Nombres: "+e.getNombre());
+           System.out.println("LU: "+e.getLU());
+           System.out.println("Email: "+e.getEmail());
+           System.out.println("Regular: "+e.regular);
+           System.out.println("Nota 1:"+e.getNotaP1());
+           System.out.println("Nota 2:"+e.getNotaP2());
+           System.out.println("Nota R:"+e.getNotaR());
+           System.out.println("Asistencias: "+e.getAsistencia());
+           System.out.println("--------------------------");
+           
+       }
+    }
+   private void ordenarLista(){
+       System.out.println("Lista Ordenada");
+       for(int i=0 ; i<this.totalEstudiante-1 ; i++){
+           
+            for (int j=i+1 ; j<this.totalEstudiante ; j++){
+                
+                if (this.listaEstudiantes[i].getApellido().compareTo(this.listaEstudiantes[j].getApellido())>0){
+                    
+                    Estudiante auxiliar = this.listaEstudiantes[j];
+                    this.listaEstudiantes[j] = this.listaEstudiantes[i];
+                    this.listaEstudiantes[i] = auxiliar;
+                }
+            }
+       }
+   }
+   
+   public Estudiante buscarEstudiante(int lu){
+       int i=0;
+       boolean encontrado = false;
+       while (i<this.totalEstudiante || encontrado!=true){
+           if (this.listaEstudiantes[i].getLU() == lu){
+               encontrado = true;   
+           }
+       }
+       if (encontrado){
+          return this.listaEstudiantes[i];
+       }
+       else return null;
+   }
+   
+   public void mostrarRegulares(){
+       for (int i=0 ; i<this.totalEstudiante ; i++){
+           if (this.listaEstudiantes[i].regular == true){
+               Estudiante e = this.listaEstudiantes[i];
+               System.out.println("Estudiante #" + (i+1));
+               System.out.println("Apellido: "+e.getApellido());
+               System.out.println("Nombres: "+e.getNombre());
+               System.out.println("LU: "+e.getLU());
+               System.out.println("Email: "+e.getEmail());
+               System.out.println("Regular: "+e.regular);
+               System.out.println("Nota 1:"+e.getNotaP1());
+               System.out.println("Nota 2:"+e.getNotaP2());
+               System.out.println("Nota R:"+e.getNotaR());
+               System.out.println("Asistencias: "+e.getAsistencia());
+               System.out.println("--------------------------");           
+
+           }
+       }
+   }
+   
 }
