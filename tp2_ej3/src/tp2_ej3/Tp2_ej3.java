73f1055e404f6e5c4f97afba2e47a8a2b24ae417
David <David@LAPTOP-887LF5BD>
2023-04-16 23:58:25 -0300

    ej3

---
+++ tp2_ej3/src/tp2_ej3/Tp2_ej3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_ej3;
+
+/**
+ *
+ * @author David
+ */
+public class Tp2_ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Estudiante e1;
+        
+       
+    }
+    
+}
