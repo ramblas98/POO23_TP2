8624331308b4b51f1cacbbe90d91a6e270f49bfd
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-04-18 21:46:53 -0300

    TPN6

---
+++ TP2_EJ6/src/main/java/com/mycompany/tp2_ej6/CuadroMedico.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_ej6;
+
+
+public class CuadroMedico {
+    private Medico[] listaMedicos;
+
+    public CuadroMedico(){
+    
+    }
+
+    public CuadroMedico(Medico[] listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+
+    public Medico[] getListaMedicos() {
+        return listaMedicos;
+    }
+
+    public void setListaMedicos(Medico[] listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+    
+
+}
