8013f6bfe2a79d31b487869a8877bdcc5781b1e8
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-04-25 22:38:10 -0300

    TP7

---
+++ TP2_EJ7/src/tp2_ej7/Deposito.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej7;
+
+
+public class Deposito {
+    private Cliente titular;
+    private double capital;
+    private int plazo;
+    private double finalTNA;
+    private double finalTEA;
+
+    public Deposito() {
+    }
+
+    public Deposito(Cliente titular, double capital, int plazo, double finalTNA, double finalTEA) {
+        this.titular = titular;
+        this.capital = capital;
+        this.plazo = plazo;
+        this.finalTNA = finalTNA;
+        this.finalTEA = finalTEA;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public double getCapital() {
+        return capital;
+    }
+
+    public int getPlazo() {
+        return plazo;
+    }
+
+    public double getFinalTNA() {
+        return finalTNA;
+    }
+
+    public double getFinalTEA() {
+        return finalTEA;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
+
+    public void setPlazo(int plazo) {
+        this.plazo = plazo;
+    }
+
+    public void setFinalTNA(double finalTNA) {
+        this.finalTNA = finalTNA;
+    }
+
+    public void setFinalTEA(double finalTEA) {
+        this.finalTEA = finalTEA;
+    }
+    
+    
+}
